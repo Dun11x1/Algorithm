db5000eb3d84b7b6b0c5b5964b1c5cc3d19c1ddd
Dun11x1 <96408972+Dun11x1@users.noreply.github.com>
2023-12-27 21:45:12 +0900

    Add files via upload

---
+++ Algorithm1.py
@@ -0,0 +1,18 @@
+#ex1
+print("hello world")
+
+#ex2
+str = "hello world"
+print(str)
+
+#ex3
+str1 = "hello"
+str2 = "world"
+greet = str1 + " " + str2
+print(greet)
+
+#ex4
+def print_string(input_string):
+    print(input_string)
+#call the function with ex3 variable
+print(greet)
